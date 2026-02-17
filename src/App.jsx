fcef2cc575a5041fe6579dc9ec445d7bd726e23b
55Pranjal <567pranjalagarwal@gmail.com>
2026-02-17 20:09:30 +0530

    Made background doodle style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-
 import "./App.css";
 
 import Login from "./components/login.jsx";
@@ -19,27 +18,35 @@ import CommunityChatPage from "./components/CommunityChatPage.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/profileDecision" element={<ProfileDecision />} />
-      <Route path="/onboarding" element={<Onboarding />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/discover" element={<DiscoverPage />} />
-      <Route path="/public/:id" element={<PublicProfilePage />} />
-      <Route path="/connections" element={<ConnectionsPage />} />
-
-      <Route path="/dm/:conversationId" element={<DMChatPage />} />
-
-      <Route path="/communities" element={<CommunityPage />} />
-      <Route path="/create-community" element={<CreateCommunity />} />
-      <Route path="/community/:communityId" element={<ChannelPage />}>
-        <Route path="channel/:channelId" element={<CommunityChatPage />} />
-      </Route>
-
-      <Route path="*" element={<div>NO ROUTE MATCHED</div>} />
-    </Routes>
+    <div
+      className="min-h-screen"
+      style={{
+        backgroundColor: "#ffffff",
+        backgroundImage: "url('/doodles.svg')",
+        backgroundRepeat: "repeat",
+        backgroundSize: "320px",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profileDecision" element={<ProfileDecision />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/discover" element={<DiscoverPage />} />
+        <Route path="/public/:id" element={<PublicProfilePage />} />
+        <Route path="/connections" element={<ConnectionsPage />} />
+        <Route path="/dm/:conversationId" element={<DMChatPage />} />
+        <Route path="/communities" element={<CommunityPage />} />
+        <Route path="/create-community" element={<CreateCommunity />} />
+        <Route path="/community/:communityId" element={<ChannelPage />}>
+          <Route path="channel/:channelId" element={<CommunityChatPage />} />
+        </Route>
+        <Route path="*" element={<div>NO ROUTE MATCHED</div>} />
+      </Routes>
+    </div>
   );
 }
 
